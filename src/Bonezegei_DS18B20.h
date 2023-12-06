d0a164b195a5aef75dbe6060a40e5d98a2ded276
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-12-06 09:02:31 +0800

    Update Bonezegei_DS18B20.h

--- src/Bonezegei_DS18B20.h
+++ src/Bonezegei_DS18B20.h
@@ -1,5 +1,15 @@
 /*
   DS18B20 Library
   Author: Bonezegei (Jofel Batutay)
-  Date : November 2023
+  Date : 
 */
+#ifndef _BONEZEGEI_DS18B20_H_
+#define _BONEZEGEI_DS18B20_H_
+#include <Arduino.h>
+
+class Bonezegei_DS18B20{
+public:
+  Bonezegei_DS18B20();
+};
+
+#endif
